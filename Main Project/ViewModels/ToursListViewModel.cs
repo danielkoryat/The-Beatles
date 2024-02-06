e38feaa8793cf9099f6dee8fe3abe0d97bf04eb6
danielkoryat <dan.koryat@gmail.com>
2024-02-06 03:02:40 +0200

    Add GetTimeLeft method to ToursListViewModel and update _ToursList.cshtml and styles.css
    - Add GetTimeLeft method to ToursListViewModel to calculate the time left until a tour date. The method returns a string with the number of days, hours, and minutes left until the tour date or a message indicating that the date has already passed.
    - Update _ToursList.cshtml to display the time left until the tour date. The tour date is now wrapped in a span element

--- Main Project/ViewModels/ToursListViewModel.cs
+++ Main Project/ViewModels/ToursListViewModel.cs
@@ -7,5 +7,18 @@ namespace Main_Project.ViewModels
         public List<Tour> Tours { get; set; }
 
         public List<int> PurchasedToursIds { get; set; }
+
+        public string GetTimeLeft(DateTime tourDate)
+        {
+            TimeSpan timeLeft = tourDate - DateTime.Now;
+            if (timeLeft.TotalSeconds > 0)
+            {
+                return $"{timeLeft.Days} days, {timeLeft.Hours} hours, {timeLeft.Minutes} minutes left";
+            }
+            else
+            {
+                return "The date has already passed.";
+            }
+        }
     }
 }
